b433e24bad513872b2f18b76da6d880961102a13
SubhamayDebnath <subhamaydebnath6@gmail.com>
2024-03-06 20:46:51 +0530

    first commit

---
+++ BinarySearch/question_1.py
@@ -0,0 +1,39 @@
+class BinaryList:
+    def __init__(self,arr,dataType=int):
+        self.itemArr=arr
+        self.arrSize=len(self.itemArr)
+        self.dataType=dataType
+        for j in self.itemArr:
+            if type(j)!=dataType:
+                print("not same type of data")
+                exit()
+        if self.itemArr[0] > self.itemArr[1]:
+            print("not sorted")
+            exit()
+    def display(self):
+        print(self.itemArr)
+    def binarySearch(self,searchItem):
+        beg=0
+        end=self.arrSize-1
+        while beg <= end:
+            mid=(beg+end)//2
+            if (searchItem == self.itemArr[mid]):
+                print(f"index of {searchItem} is {mid}")
+                return mid
+            elif searchItem < self.itemArr[mid]:
+                end=mid-1
+                print(end)
+            else:
+                beg=mid+1
+    def insertItem(self,pos,insertItem):
+        if self.itemArr [pos-1] > insertItem:
+             print("not allowed")
+        else:
+            self.itemArr.insert(pos,insertItem) 
+    def deleteItem(self,delItem):
+        self.itemArr.remove(delItem)
+
+obj=BinaryList([1,2,3,4,5])
+obj.binarySearch(5)
+obj.insertItem(4,0)
+obj.display()
---
+++ LinearSearch/question_1.py
@@ -0,0 +1,27 @@
+class LinearList:
+    def __init__(self,arr,dataType=int):
+        self.itemArr=arr
+        self.arrSize=len(self.itemArr)
+        self.dataType=dataType
+        for j in self.itemArr:
+            if type(j)!=dataType:
+                print("not same type of data")
+                exit()
+    def display(self):
+        print(self.itemArr)
+    def linearSearch(self ,searchItem):
+        for i in range(self.arrSize):
+            if searchItem == self.itemArr[i]:
+                print(f"index of {searchItem} is {i}")
+                return i
+        else:
+            print(f"{searchItem} is not found")
+    def insertItem(self,pos,insertItem):
+        self.itemArr.insert(pos,insertItem)
+    def deleteItem(self,delItem):
+        self.itemArr.remove(delItem)
+obj=LinearList([1,3,9,8,7],int)
+obj.linearSearch(9)
+obj.insertItem(0,99)
+obj.deleteItem(1)
+obj.display()

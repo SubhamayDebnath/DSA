2444d8f34cd853d69bb21579f6a1bc7e911651c7
SubhamayDebnath <subhamaydebnath6@gmail.com>
2024-04-13 19:32:50 +0530

    add file

---
+++ LinkedList/question.py
@@ -0,0 +1,67 @@
+class Node:
+    def __init__(self,value):
+        self.Info=value
+        self.next=None
+class SingleLinkedList:
+    def __init__(self):
+        self.start=None
+    def insertAtLast(self,value):
+        n=Node(value)
+        if self.start==None:
+            self.start=n
+        else:
+            temp=self.start
+            while temp.next != None:
+                temp=temp.next
+            temp.next=n
+    def traverse(self):
+        n=self.start
+        while n!=None:
+            print(n.Info)
+            n=n.next
+    def insertAtbegining(self,num):
+        n=Node(num)
+        n.next=self.start
+        self.start=n
+        return n
+    def deleteLastNode(self):
+        temp=self.start
+        while temp.next != None:
+            prev=temp
+            temp=temp.next
+        del temp 
+        prev.next=None
+    def insertAfterSpecificItem(self,item,position):
+        n=Node(item)
+        temp=self.start
+        while temp.Info != position:
+            temp=temp.next
+        n.next=temp.next
+        temp.next=n
+    def deleteSpecificItem(self,position):
+        temp=self.start
+        while temp.Info != position:
+            prev=temp
+            temp=temp.next
+        prev.next=temp.next
+        del temp
+sl=SingleLinkedList()
+sl.insertAtLast(20)
+sl.insertAtLast(2)
+sl.insertAtLast(50)
+sl.insertAtLast(80)
+sl.insertAtLast(87)
+print("traverse 1")
+sl.traverse()
+sl.insertAtbegining(99)
+print("traverse 2")
+sl.traverse()
+sl.deleteLastNode()
+print("traverse 3")
+sl.traverse()
+sl.insertAfterSpecificItem(34,2)
+print("traverse 4")
+sl.traverse()
+sl.deleteSpecificItem(34)
+print("traverse 5")
+sl.traverse()
--- QuickSort/question.py
+++ QuickSort/question.py
@@ -1,39 +1,27 @@
-class QuickSort:
-    def __init__(self,arr,dataType=int):
-        self.arr=arr
-        self.dataType=dataType
-        for i in self.arr:
-            if(type(i)!=dataType):
-                print("not same type of data")
-                exit()
-    def display(self):
-        print(self.arr)
-    def quick(self,low,high):
-        pivot=self.arr[low]
-        right=self.arr[len(self.arr)-1]
-        left=1
-        while True:
-            while self.arr[pivot] <= self.arr[right] and left <= right:
-                right=right-1
-                if self.arr[pivot] >= self.arr[right]:
-                    temp=self.arr[pivot]
-                    self.arr[pivot]=self.arr[right]
-                    self.arr[right]=temp
-                    right=right+1
-                if left > right:
-                    return pivot
-            while self.arr[pivot] >= self.arr[left] and left <= right:
-                if self.arr[pivot] <= self.arr[left]:
-                    temp=self.arr[pivot]
-                    self.arr[pivot]=self.arr[left]
-                    self.arr[left]=temp
-                    left=left+1
-                if left > right:
-                    return pivot
-    def quickSort(self):
-        
+def quickSort(arr,left,right):
+    if left < right:  
+        q=quick(arr,left,right)
+        quickSort(arr,left,q-1)  
+        quickSort(arr,q+1,right)     
+def quick(arr,left,right):
+    i=left
+    j=right-1
+    pivot=arr[right]
+    while i < j:
+        while i < j and arr[i] < pivot:
+            i+=1
+        while j > i and arr[j] >=pivot:
+            j-=1
+        if i < j:
+            temp=arr[i]
+            arr[i]=arr[j]
+            arr[j]=temp
+    if arr[i] > pivot:
+        temp=arr[i]
+        arr[i]=arr[right]
+        arr[right]=temp
+    return i
+arr=[22,11,88,45,89,1]
+quickSort(arr,0,len(arr)-1)
+print(arr)
 
-
-
-
-    
